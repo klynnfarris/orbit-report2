35a014d593a79d69f6919d2f1a3d7c9e2ccb55f8
Katie <klynnfarris@gmail.com>
2019-12-18 16:39:00 -0600

    step 8 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,9 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   sourceList: Satellite[];
+  displayList: Satellite[];
 
-  // constructor() {
+  // constructor(displayList = []) {
   //   this.sourceList = [
   //      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
   //      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
@@ -34,9 +35,25 @@ export class AppComponent {
           let newSatellite =  new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
            this.sourceList.push(newSatellite);
          }
+         this.displayList = this.sourceList.slice(0);
 
      }.bind(this));
   }.bind(this));
 
 }
+
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
+
  }
